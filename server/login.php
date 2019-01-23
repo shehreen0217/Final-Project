50521d1de024b16cbcb904d91dee5a7c0a2f94fb
iamalihamza <aleeehamza@gmail.com>
2019-01-23 20:02:17 +0500

    Header file Included In all files(with INCLUDE)

--- server/login.php
+++ server/login.php
@@ -95,40 +95,9 @@
 </head>
 
 <body id="bg">
-<div id="navbar">
-    <nav class="navbar navbar-inverse" role="navigation">
-        <div class="container-fluid">
-            <!--logo-->
-
-            <div class="navbar-header">
-                <a class="navitem1" href="#" class="navbar-brand"><h1>TIB</h1> <h4>The Instant Bloggers</h4> </a>
-            </div>
-
-            <div>
-                <ul class="nav navbar-collapse">
-                    <li class="active"> <a class="navitem" href="../index.php"><i class="fa fa-home" aria-hidden="true"></i> Home </a> </li>
-                    <li> <a class="navitem" href="about_us.php"> <i class="fa fa-users" aria-hidden="true"></i> About Us </a> </li>
-
-
-                    <!--drop down list-->
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="login.php"><i class="fa fa-user-circle" aria-hidden="true"></i> Log In</a>
-                            <a class="dropdown-item" href="register.php"><i class="fa fa-book" aria-hidden="true"></i> Register</a>
-                            <a class="dropdown-item" href="forget_password.php"><i class="fa fa-frown"></i> Forgot Password</a>
-                            <a class="dropdown-item" href="contact_us.php"> <i class="fa fa-phone" aria-hidden="true"></i> Contact us</a>
-                        </div>
-                    </div>
-                </ul>
-
-            </div>
-
-        </div>
-    </nav>
-</div>
-
+<?php
+include("header.php")
+?>
 <div class="container">
     <div  class="login">
         <div class="login-header">
