bc14faad6fe681a8bdb34a7644fe6722829e8712
Muhammad Ali Sulehri <muhammadali75j@gmail.com>
2019-01-24 12:56:23 +0500

    working 2

--- admin/insert_categories.php
+++ admin/insert_categories.php
@@ -4,37 +4,18 @@ if(!isset($_SESSION['login_checked'])) {
     header('location: login.php');
 }
 
-if(isset($_POST['insert_pro'])){
-    //getting text data from the fields
-    $pro_title = $_POST['pro_title'];
-    $pro_cat = $_POST['pro_cat'];
-    $pro_brand = $_POST['pro_brand'];
-    $pro_price = $_POST['pro_price'];
-    $pro_desc = $_POST['pro_desc'];
-    $pro_keywords = $_POST['pro_keywords'];
+if(isset($_POST['insert_cat'])){
+    $cat_title = $_POST['cat_title'];
+    $cat_image = $_FILES['cat_image']['name'];
 
-    //getting image from the field
-    $pro_image = $_FILES['pro_image']['name'];
-    $pro_image_tmp = $_FILES['pro_image']['tmp_name'];
-    move_uploaded_file($pro_image_tmp,"product_images/$pro_image");
-
-    $insert_product = "insert into products (pro_cat, pro_brand,pro_title,pro_price,pro_desc,pro_image,pro_keywords) 
-                  VALUES ('$pro_cat','$pro_brand','$pro_title','$pro_price','$pro_desc','$pro_image','$pro_keywords');";
-    $insert_pro = mysqli_query($con, $insert_product);
-    if($insert_pro){
+    $insert_cat = "insert into categories (cat_title, cat_image) 
+                  VALUES ('$cat_title','$cat_image');";
+    $query2 = mysqli_query($con, $insert_cat);
+    if($insert_cat){
         header("location: ".$_SERVER['PHP_SELF']);
     }
 }
-
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">
-</head>
-
-<body>
 
 <h1 class="text-center my-4 font2"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New Category </span></h1>
 <form action="" method="post" enctype="multipart/form-data">
@@ -43,24 +24,21 @@ if(isset($_POST['insert_pro'])){
             <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Title: </label>
         </div>
         <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-auto">
-            <input type="text" class="form-control" name="title" placeholder="Enter Category Title" >
+            <input type="text" class="form-control" name="cat_title" placeholder="Enter Category Title" >
         </div>
         <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-3 mt-auto">
             <label for="pro_img" class="float-md-right"><span class="d-sm-none d-md-inline"> Image: </label>
         </div>
         <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-auto">
             <div class="input-group-prepend">
-                <input class="form-control" type="file" name="image">
+                <input class="form-control" type="file" name="cat_image">
             </div>
         </div>
     </div>
     <div class="row my-5">
         <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-4 mt-auto"></div>
         <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
-            <button type="submit" name="insert_pro" class="btn btn_s btn-block"><i class="fas fa-plus"></i> Insert Now </button>
+            <button type="submit" name="insert_cat" class="btn btn_s btn-block"><i class="fas fa-plus"></i> Insert Now </button>
         </div>
     </div>
 </form>
-
-</body>
-</html>
--- admin/view_categories.php
+++ admin/view_categories.php
@@ -5,16 +5,50 @@ if(!isset($_SESSION['login_checked'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-</head>
-
-
-<body>
-
-<div>yo</div>
-
-</body>
-</html>
+<div class="row">
+    <div class="col-sm-12">
+        <h1 align="center">Categories</h1>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+            </tr>
+            </thead>
+            <?php
+            $get_cat = "select * from categories";
+            $run_cat = mysqli_query($con,$get_cat);
+            $count_cat = mysqli_num_rows($run_cat);
+            if($count_cat==0){
+                echo "<h2> No Category exist. </h2>";
+            }
+            ?>
+            else {
+            <thead>
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+            </tr>
+            </thead>
+            <?php
+            $i = 1;
+                while ($row_pro = mysqli_fetch_array($run_cat)) {
+                    $cat_title = $row_pro['cat_title'];
+                    $cat_image = $row_pro['cat_image'];
+                    ?>
+                    <tr>
+                        <th><?php echo $cat_title; ?></th>
+                        <th><img class="img-thumbnail" src='product_images/<?php echo $cat_image;?>' width='80' height='80'></th>
+                        <th><a href="index.php?edit_pro=<?php echo $cat_title?>" class="btn btn-primary">
+                                <i class="fa fa-edit"></i> Edit
+                            </a>
+                            <a href="index.php?del_pro=<?php echo $cat_title?>" class="btn btn-danger">
+                                <i class="fa fa-trash-alt"></i> Delete
+                            </a>
+                        </th>
+                    </tr>
+                <?php}?>
+            }
+        </table>
+    </div>
+</div>
