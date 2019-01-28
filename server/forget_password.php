4372898f4f10a1a4253abf3e69f195807ac6834a
iamalihamza <aleeehamza@gmail.com>
2019-01-28 23:29:56 +0500

    Forget Password page updated

--- server/forget_password.php
+++ server/forget_password.php
@@ -23,66 +23,74 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <?php
+require "tib_db.php";
+?>
+    <script src="/admin/js/jquery-3.3.1.js"></script>
+    <script>
+        function password_match() {
+            var password=document.getElementById('password').value;
+
+            var cpassword=document.getElementById('cpassword').value;
+            $.post("check.php",
+                {
+                    pass1:password,pass2:cpassword
+                },
+                function (data,status) {
+                    document.getElementById('show_res').innerHTML=data;
+
+                }
+            )
+
+        }
+    </script>
+<?php
+global $con;
+
+if(!$con)
+{
+    echo "Not Connected";
+}
+if(!mysqli_select_db($con,'tib'))
+{
+    echo "DB not selected";
+}
+
+if(isset($_POST['submitInfo']))
+{
+        $yourEmail=$_POST['yourEmail'];
+        $password=$_POST['password'];
+        $selectQuery="select * from users where u_email='$yourEmail'";
+        $count=mysqli_query($con,$selectQuery);
+        $row=mysqli_num_rows($count);
+        while($row>0)
+        {
+            $sQuery = mysqli_query($con,"UPDATE users SET u_password='$password' where u_email='$yourEmail'");
+            break;
+            /*echo "<script>alert('Email Does not exist!')</script>";*/
+        }
+}?>
+
 </head>
 <body id="BG">
 <?php
 include("header.php");
 ?>
-<form action="" method="post">
+<form action="forget_password.php" method="post">
 <h4 class="fp"><i class=" 	far fa-question-circle"></i> Forgot Password</h4>
 <div class="box">
-    <input class="int" type="text"  id=youremail name=youremail placeholder="Your email:"><br>
-    <button class="Code"type="button" name="sendcode"> Send Code</button><br>
-    <input class="int" type="text" placeholder="Code we sent:"><br>
+    <input class="int" type="text"  id=yourEmail name=yourEmail placeholder="Your email:"><br>
+    <button class="Code" type="button" name="sendCode"> Send Code</button><br>
+     <input class="int" type="text" placeholder="Code we sent:"><br>
     <h3 class="new"> Reset Password</h3>
     <input class="int" type="password" name="password" id="password" placeholder="New Password:"><br>
     <input class="int" type="password" name="cpassword" id="cpassword" placeholder="Confirm Password"><br>
-    <button class="Submit" id="button" name="submit" type="button" onclick="password_match()">SUBMIT</button><br><br>
+    <button class="Submit" id="button" name="submitInfo" type="submit" onclick="password_match()">SUBMIT</button><br><br>
     <div id="show_res"></div>
 </div>
 </form>
-<script src="/admin/js/jquery-3.3.1.js"></script>
-<script>
-    function password_match() {
-        var password=document.getElementById('password').value;
 
-        var cpassword=document.getElementById('cpassword').value;
-        $.post("check.php",
-            {
-                pass1:password,pass2:cpassword
-            },
-            function (data,status) {
-            document.getElementById('show_res').innerHTML=data;
-
-            }
-        )
-    }
-</script>
 <?php
-require "tib_db.php";
-
-if(isset($_POST['sendcode']))
-{
-    //if($_POST)
-    //{
-        $youremail=$_POST['youremail'];
-        $password=$_POST['password'];
-        $selectquery=mysqli_query($con,"select * from users where u_email='{$youremail}'")or die(mysqli_error($con));
-        $count=mysqli_query($con,$selectquery);
-        $row=mysqli_num_rows($count);
-        if($row>0)
-        {
-            $squery=mysqli_query($con,"update users SET u_password=$password where u_email=$youremail");
-        }
-        else
-        {
-            echo "<script>alert('incorrect Email!')</script>";
-        }
-    //}
-}
-
-
-
 include ("footer.php");
 ?>
 
